0ba2ab8be13755cc9911a0cebcdacb533c2214ed
Puja Borse <pujaborse13@gmail.com>
2024-11-25 14:40:07 +0530

    UC3: Add Employee Part Time

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -8,22 +8,26 @@ public class Main {
         System.out.println("Welcome to Employee wage Computation Program");
 
         final int WAGE_PER_HOUR = 20;
-        final int FULL_DAY_HOURS =8;
-
+        final int FULL_DAY_HOURS = 8;
+        final int PART_TIME_HOURS = 4;
 
         Random random = new Random();
-        int attendance = random.nextInt(2);
+        int employeeType = random.nextInt(3);
 
-        if(attendance == 1 )
-        {
+        if (employeeType == 1) {
             int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Full Time");
             System.out.println("Daily Wage: $" + dailyWage);
+        } else if (employeeType == 2) {
+            int dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            System.out.println("Employee is Part-Time");
+            System.out.println("Daily Wage: $" + dailyWage);
+
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Wage: $0");
+        }
 
-                }
 
-
-          }
     }
+}
