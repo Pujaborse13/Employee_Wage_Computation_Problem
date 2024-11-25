4040b8bf5b2c3772fbee5047962c934d1e4fe57d
Puja Borse <pujaborse13@gmail.com>
2024-11-25 11:15:43 +0530

    UC1: Added employee attendance check using Random

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
-        }
+                }
 
 
           }
