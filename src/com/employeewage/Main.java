cb062dc4dc56c227dd919b83142a5c8799b39ffc
Puja Borse <pujaborse13@gmail.com>
2024-11-25 11:44:13 +0530

    UC2: Calculated daily employee wage based on attendance

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -7,14 +7,21 @@ public class Main {
 
         System.out.println("Welcome to Employee wage Computation Program");
 
+        final int WAGE_PER_HOUR = 20;
+        final int FULL_DAY_HOURS =8;
+
+
         Random random = new Random();
         int attendance = random.nextInt(2);
 
         if(attendance == 1 )
         {
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
             System.out.println("Employee is Present");
+            System.out.println("Daily Wage: $" + dailyWage);
         } else {
             System.out.println("Employee is Absent");
+
                 }
 
 
