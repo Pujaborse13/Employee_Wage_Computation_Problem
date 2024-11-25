fb63b6100f3b734579ddc3831c5ccf046f8744a1
Puja Borse <pujaborse13@gmail.com>
2024-11-25 11:04:55 +0530

    UC1: Added employee attendance check using Random

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -1,9 +1,22 @@
 package com.employeewage;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee wage Computation Program");
-        //https://github.com/Pujaborse13/Employee_Wage_Computation_Problem.git
-    }
+
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+
+        if(attendance == 1 )
+        {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+
+          }
     }
