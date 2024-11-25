ab566ddb953538edfa11963adf8802bd861d4cb1
Puja Borse <pujaborse13@gmail.com>
2024-11-25 10:42:18 +0530

    Added to master

---
+++ src/com/employeewage/Main.java
@@ -0,0 +1,9 @@
+package com.employeewage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee wage Computation Program");
+        //https://github.com/Pujaborse13/Employee_Wage_Computation_Problem.git
+    }
+    }
