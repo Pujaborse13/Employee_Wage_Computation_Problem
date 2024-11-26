cb0e40d6d3808fd6945f2be9f26e282c1ee4e1e4
Puja Borse <pujaborse13@gmail.com>
2024-11-26 10:30:50 +0530

    UC6: Added wage calculation until 100 hours or 20 days condition is met

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -11,28 +11,45 @@ public class Main {
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOURS = 8;
         final int PART_TIME_HOURS = 4;
-        final int WORKING_DAYS_PER_MONTH = 20;
+        final int WORKING_DAYS_LIMIT = 20;
+        final int WORKING_HOURS_LIMIT = 100;
 
         int totalWage = 0;
+        int totalHoursWorked = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+
+        while (totalWorkingDays < WORKING_DAYS_LIMIT && totalHoursWorked < WORKING_HOURS_LIMIT) {
+            totalWorkingDays++;
+
             int dailyHours = getWorkingHours();
+            totalHoursWorked += dailyHours;
+            if (totalHoursWorked > WORKING_HOURS_LIMIT) {
+                totalHoursWorked -= dailyHours;
+                break;
+            }
+
             int dailyWage = dailyHours * WAGE_PER_HOUR;
             totalWage += dailyWage;
-            System.out.println("Day " + day + ": Worked " + dailyHours + " hours, Earned: $" + dailyWage);
-        }
-
-        System.out.println("Total Wage for the Month: $" + totalWage);
+            System.out.println("Day " + totalWorkingDays + ": Worked " + dailyHours + " hours, Earned: " + dailyWage);
     }
+
+        System.out.println("\nTotal Working Days: " + totalWorkingDays);
+        System.out.println("Total Hours Worked: " + totalHoursWorked);
+        System.out.println("Total Wage for the Month: " + totalWage);
+    }
+
     public static int getWorkingHours() {
-        int empCheck = (int) (Math.random() * 3);
+        int empCheck = (int) (Math.random() * 3); // Random value: 0, 1, or 2
         switch (empCheck) {
             case 1: return 8;
             case 2: return 4;
             default: return 0;
-              }
+
+
 
         }
+    }
 
 
     }
